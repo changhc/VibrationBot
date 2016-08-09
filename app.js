6c8d184fc9e3bb120c884bb516a70c7ab66a83db
Charlie Chang (Adecco/Student) <v-ccharl@microsoft.com>
2016-08-09 15:10:10 +0800

    test

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+/*****LICENSE HERE*****/
+/******************
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*******************/
+
 // Add your requirements
 var restify = require('restify'); 
 var builder = require('botbuilder'); 
@@ -21,73 +30,48 @@ server.get('/', restify.serveStatic({
 }));
 var sess;
 server.post('/testestest', function(req, res){
-	//console.log(bot.connectors);
 	if(bot != null){
 		
 		var msg = req;
-		
-		//bot.beginDialog();
 	
-	sql.connect(config, function(err) {
-		if(err) {
-			session.send("DB ERROR");
-			session.endDialog();
-		}
-		//console.log(err);
-		var request = new sql.Request();
-		
-		request.query("SELECT * FROM AlertSubscription;", function(err, recordset){
-			console.log("size = " + recordset.length);
-			for(i = 0; i < recordset.length; ++i){
-				//console.log(sess.message.address);
-				try{
-					//var msg = new builder.Message();
-					var addr = {
-						'channelId': recordset[i].ChannelId,
-						'conversation': {
-							'id': recordset[i].ConvId
-						},
-						'user': {
-							'id': recordset[i].UserId
-						},
-						'bot': {
-							'id': recordset[i].BotId
-						},
-						'serviceUrl': recordset[i].ServiceURL,
-						'useAuth': true
-					};
-					//msg.address(addr);
-					console.log(addr);
-					/*
-				sess.message.address.channelId = 
-				sess.message.address.conversation.id = ;
-				sess.userData.name = recordset[i].UserName;
-				sess.message.address.user.id = recordset[i].RecpId;
-				sess.message.address.user.name = recordset[i].RecpName;
-				sess.message.address.serviceUrl = recordset[i].ServiceURL;
-				sess.send(sess.message.address.id==null);
-				//console.log(sess);
-				sess.send("DeviceId: %s\r\ntemp: %s\r\nspeed: %s\r\ntime: %s", msg.body.deviceid, msg.body.temp, msg.body.speed, msg.body.time);
-				*/
-					//bot.connector(recordset[i].ChannelId, connector);
-					bot.beginDialog(addr, '/alert', msg);
-					//msg.text(req);
-
-						//var tmp = bot.send(msg);
-						//console.log(tmp);
-
-					
-				}
-				catch(err){
-					console.log(err);
-				}
+		sql.connect(config, function(err) {
+			if(err) {
+				session.send("DB ERROR");
+				session.endDialog();
 			}
+			//console.log(err);
+			var request = new sql.Request();
+			
+			request.query("SELECT * FROM AlertSubscription;", function(err, recordset){
+				for(i = 0; i < recordset.length; ++i){
+					try{
+						var addr = {
+							'channelId': recordset[i].ChannelId,
+							'conversation': {
+								'id': recordset[i].ConvId
+							},
+							'user': {
+								'id': recordset[i].UserId
+							},
+							'bot': {
+								'id': recordset[i].BotId
+							},
+							'serviceUrl': recordset[i].ServiceURL,
+							'useAuth': true
+						};
+						bot.beginDialog(addr, '/alert', msg);
+
+					}
+					catch(err){
+						console.log(err);
+					}
+				}
+				
+			});
 			
 		});
 		
-	});
-	//sess.send("DeviceId: %s\r\ntemp: %s\r\nspeed: %s\r\ntime: %s", msg.body.deviceid, msg.body.temp, msg.body.speed, msg.body.time);
-	res.send(202);
+		res.send(202);
 	}
 });
 
@@ -103,145 +87,138 @@ var config = {
     options: {
         encrypt: true // Use this if you're on Windows Azure
     }
-}
+};
+
 //var connector = new builder.ConsoleConnector().listen();		//console test 
 var bot = new builder.UniversalBot(connector);
 
 bot.dialog('/', [
 
 	function (session) {
-		sess = session;
-		session.send(JSON.stringify(session.message.address));
-		builder.Prompts.number(session, "Hi " + session.userData.name + ", what would you like to know about?\n\r1. The agent ID of the latest submission\n2. The latest submission time\n3. The latest temperature data\n4. The latest speed data");
+		builder.Prompts.number(session, "Hi " + session.userData.name + ", what would you like to know about?\n\r1. Current status\n2. Previous warnings\n3. Unsubscribe");
 	},
 	function(session, results){
-		if(results.response == 1){
-			if(session.userData.ReceiveAlert){
-				session.send("You have subscribed.");
-				session.endDialog();
-				//session.beginDialog('/');
-			}
-			else{
-				session.send("ok");
+		if(!session.userData.ReceiveAlert && session.message.address.channelId == 'skype'){
+			var query = "INSERT INTO AlertSubscription (ChannelId, ConvId, UserName, BotId, UserId, ServiceURL) VALUES ('" + session.message.address.channelId + "', '" + session.message.address.conversation.id + "', '" + session.userData.name + "', '" + session.message.address.bot.id + "', '" + session.message.address.user.id + "', '" + session.message.address.serviceUrl + "');";
+			sql.connect(config, function(err) {
+				if(err) {
+					session.send("DB ERROR");
+					session.endDialog();
+				}
+				var request = new sql.Request();
 				
-				var query = "INSERT INTO AlertSubscription (ChannelId, ConvId, UserName, BotId, UserId, ServiceURL) VALUES ('" + session.message.address.channelId + "', '" + session.message.address.conversation.id + "', '" + session.userData.name + "', '" + session.message.address.bot.id + "', '" + session.message.address.user.id + "', '" + session.message.address.serviceUrl + "');";
-				//console.log(typeof(query));
-				sql.connect(config, function(err) {
-					if(err) {
-						session.send("DB ERROR");
-						session.endDialog();
-					}
-					var request = new sql.Request();
-					
-					request.query(query, function(err, recordset){
-						console.log(err);
-						if(err == undefined) session.userData.ReceiveAlert = true;
-						//session.send(recordset);
-						
-						//session.send(msg + value + '.');
-						session.endDialog();
-						//session.beginDialog('/');
-					});
-					
+				request.query(query, function(err, recordset){
+					console.log(err);
+					if(err == undefined) session.userData.ReceiveAlert = true;
+					session.send("You are now subscribed.");
 				});
-			}
+				
+			});
 		}
+		if(results.response == 1) {
+			var value, time;
+			sql.connect(config, function(err) {
+				if(err) {
+					session.send("DB ERROR");
+					session.endDialog();
+				}
+				//console.log(err);
+				var request = new sql.Request();
+				var query = 'SELECT TOP 1 Chl_1_Overall,Chl_2_Overall,Channel_1_gSE_Overall,Channel_2_gSE_Overall,Result,Timestamp FROM Vibration ORDER BY Timestamp DESC';
+				request.query(query, function(err, recordset){
+					console.log(recordset[0].Result);
+					session.send("Status at " + recordset[0].Timestamp + " is " + recordset[0].Result + ". \n\rChl_1_Overall: " + recordset[0].Chl_1_Overall + "\n\rChl_2_Overall: " + recordset[0].Chl_2_Overall + "\n\rChannel_1_gSE_Overall: " + recordset[0].Channel_1_gSE_Overall + "\n\rChannel_2_gSE_Overall: " + recordset[0].Channel_2_gSE_Overall);
+
+					session.endDialog();
+					session.beginDialog('/');
+
+				});
+				
+			});
+
+		}
+		else if( results.response == 2 ){
+			session.endDialog();
+			session.beginDialog('/query-interval');
+		}
+		else if(results.response == 3){
+			var query = "DELETE FROM AlertSubscription WHERE UserId = " + session.message.address.user.id;
+			console.log(session.message.address.user.id);
+			sql.connect(config, function(err) {
+				if(err) {
+					session.send("DB ERROR");
+					session.endDialog();
+				}
+				var request = new sql.Request();
+				
+				request.query(query, function(err, recordset){
+					console.log(err);
+					if(err == undefined) session.userData.ReceiveAlert = true;
+					session.send("You have unsubscribed.");
+					session.endDialog();
+					session.beginDialog('/');
+				});
+				
+			});
+			
+		}
+			
+		else {
+			session.send("I can't understand. Try again.");
+			session.endDialog();
+			session.beginDialog('/');
+		}
+
 	}
-	/*
+	
+]);
+bot.dialog('/query-interval', [
+	function(session){
+		builder.Prompts.number(session, "How long would you like to query? 1 - 7 days, 0 to quit");
+	},
 	function(session, results){
 		if(results.response){
-			if(results.response <=4 && results.response >= 1) {
-
-				var config = {
-					user: process.env.DB_USER,
-					password: process.env.DB_PW,
-					server: process.env.DB_SERVER, // You can use 'localhost\\instance' to connect to named instance
-					database: process.env.DB_NAME,
-					//stream: true, // You can enable streaming globally
-
-					options: {
-						encrypt: true // Use this if you're on Windows Azure
-					}
-				}
-				console.dir(config);
-				var query, msg;
-				switch(results.response){
-					case 1:
-						query = 'SELECT TOP 1 DeviceId FROM Data ORDER BY time DESC';
-						msg = "The agent ID of the latest submission is ";
-						break;
-					case 2:
-						query = 'SELECT TOP 1 time FROM Data ORDER BY time DESC';
-						msg = "The latest submission time is ";
-						break;
-					case 3:
-						query = 'SELECT TOP 1 temp FROM Data ORDER BY time DESC';
-						msg = "The latest temperature data returned is ";
-						break;
-					case 4:
-						query = 'SELECT TOP 1 speed FROM Data ORDER BY time DESC';
-						msg = "The latest speed data returned is ";
-						break;
-					default:
-				}
-				
-				var value;
-				
+			if(results.response >= 1 && results.response <= 7){
+				var query = 'SELECT TOP ' + results.response.toString() + ' Result,Timestamp FROM Vibration ORDER BY Timestamp DESC';
+				console.log(query);
+				//config.stream = true;
 				sql.connect(config, function(err) {
 					if(err) {
 						session.send("DB ERROR");
 						session.endDialog();
 					}
-					console.log(err);
 					var request = new sql.Request();
 					
 					request.query(query, function(err, recordset){
-						console.log(err);
-						value = recordset;
-						switch(results.response){
-							case 1:
-								value = value[0].DeviceId;
-								break;
-							case 2:
-								value = value[0].time;
-								break;
-							case 3:
-								value = value[0].temp;
-								break;
-							default:
-								value = value[0].speed;
-						}
-						session.send(msg + value + '.');
-						session.endDialog();
-						session.beginDialog('/');
+						console.log(recordset.length);
+						send_msg(recordset, session, function(session){
+							session.endDialog();
+							session.beginDialog('/');
+						});
 					});
 					
 				});
-
-			}
+				//config.stream = false;
 				
-			else {
-				session.send("I can't understand. Try again.");
-				session.endDialog();
-				session.beginDialog('/');
 			}
-
+			else{
+				session.send("Invalid input. Try Again.");
+				session.endDialog();
+				session.beginDialog('/query-interval');
+			}
 		}
 		else{
 			session.endDialog();
 			session.beginDialog('/');
 		}
-	}*/
-]);
+	}]
+);
 
 bot.dialog('/alert', function(session){
-	//console.log(session);
 	try{
-		//console.log(session.options.dialogArgs.params);
 		var msg = session.options.dialogArgs.params;
-	//console.log(session.options.dialogArgs.params);
-	session.send("DeviceId: %s\r\ntemp: %s\r\nspeed: %s\r\ntime: %s", msg.deviceid, msg.temp, msg.speed, msg.time);
-	session.endDialog();
+		session.send("WARNING: " + msg.Result + " at " + msg.Timestamp);
+		session.endDialog();
 	} catch(err){
 		console.log(err);
 	}
@@ -268,3 +245,14 @@ bot.dialog('/firstRun', [
 	}
 ]);
 
+function send_msg(recordset, session, callback){
+	var msg;
+	if(recordset != null){
+		msg = recordset[0].Result + " at " + recordset[0].Timestamp + '\n\r';
+		for(i = 1; i < recordset.length; ++i){
+			msg += (recordset[i].Result + " at " + recordset[i].Timestamp + '\n\r');
+		}
+	}
+	session.send(msg);
+	callback(session);
+}
