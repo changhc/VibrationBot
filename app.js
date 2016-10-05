4204ad2e9abeefa62f58c338a3a0fe8e8beea2e8
Charlie Chang (Adecco/Student) <v-ccharl@microsoft.com>
2016-10-05 12:22:00 +0800

    timestring revised

--- app.js
+++ app.js
@@ -59,7 +59,9 @@ server.post('/testestest', function(req, res){
 							'serviceUrl': recordset[i].ServiceURL,
 							'useAuth': true
 						};
-						var text = "WARNING: " + msg.body.result + " at " + msg.body.timestamp + "\n\rProbability Score:\n\rNormal: " + msg.body.normal + "\n\rResonanceR: " + msg.body.resonancer + "\n\rResonanceU: " + msg.body.resonanceu + "\n\rUnbalanced1: " + msg.body.unbalanced1 + "\n\rUnbalanced3: " + msg.body.unbalanced3;
+						var time = Date.parse(msg.body.timestamp);
+
+						var text = "WARNING: " + msg.body.result + " at " + time.toLocaleString() + "\n\rProbability Score:\n\rNormal: " + msg.body.normal + "\n\rResonanceR: " + msg.body.resonancer + "\n\rResonanceU: " + msg.body.resonanceu + "\n\rUnbalanced1: " + msg.body.unbalanced1 + "\n\rUnbalanced3: " + msg.body.unbalanced3;
 						var message = {
 							address: addr,
 							user: addr.user,
