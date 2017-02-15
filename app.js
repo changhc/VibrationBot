49d2d84d27145bad746403ba64b32487c022d7d1
changhc <changhc84@gmail.com>
2017-02-15 11:52:30 +0800

    class names modified

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 /*****LICENSE HERE*****/
 /******************
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright © 2017 Microsoft
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 *******************/
 
 // Add your requirements
@@ -61,7 +62,7 @@ server.post('/testestest', function(req, res){
 						};
 						var time = new Date(msg.body.timestamp);
 
-						var text = "WARNING: " + msg.body.result + " at " + time.toLocaleString() + " (UTC)\n\rProbability Score:\n\rNormal: " + msg.body.normal + "\n\rResonanceR: " + msg.body.resonancer + "\n\rResonanceU: " + msg.body.resonanceu + "\n\rUnbalanced1: " + msg.body.unbalanced1 + "\n\rUnbalanced3: " + msg.body.unbalanced3;
+						var text = "WARNING: " + msg.body.result + " at " + time.toLocaleString() + " (UTC)\n\rProbability Score:\n\rNormal: " + msg.body.normal + "\n\rFan: " + msg.body.fan + "\n\rUnbalanced: " + msg.body.unbalanced;
 						var message = {
 							address: addr,
 							user: addr.user,
